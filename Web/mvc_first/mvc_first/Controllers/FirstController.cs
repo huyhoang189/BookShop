972b3a60a78569f1a805361bcdd3d4dec09e4a70
huyhoang189 <vohuyhoang189@gmail.com>
2019-09-26 22:24:46 +0700

    first

---
+++ Web/mvc_first/mvc_first/Controllers/FirstController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_first.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+       // public ActionResult Index()
+        //{
+         //   return View();
+        //}
+        public ActionResult Index()
+        {
+            DateTime a = DateTime.Now;
+            return View();
+        }
+        public string Hello()
+        {
+            return "Hello world";
+        }
+        public string showName_Age(string name ="Hoang", int age = 1)
+        {
+            return HttpUtility.HtmlEncode("Xiin chao " + name + " tuoui " + age);
+        }
+    }
+}
